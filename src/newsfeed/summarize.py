298ed1041c88021ae84ccc60cd16d0138834edd2
root <root@DESKTOP-URA6TOB>
2023-08-23 15:24:39 +0200

    summarize functionality

--- src/newsfeed/summarize.py
+++ src/newsfeed/summarize.py
@@ -1,6 +1,24 @@
 import os
 
+import openai
+from dotenv import load_dotenv
 
-def summarize_text(blog_text):
-    summary = ...  # Your code goes here
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+openai.api_key = OPENAI_API_KEY
+print(OPENAI_API_KEY)
+
+
+def summarize_text(blog_text, token=4000):
+    prompt = f"Summerize the following text very consise: '{blog_text}'"
+
+    response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=token)
+
+    summary = response.choices[0].text.strip()
     return summary
+
+
+if __name__ == "__main__":
+    blog_text = input("Enter the blog text that you want to summarize:\n")
+    summary = summarize_text(blog_text)
+    print("\nGenerated Summary:", summary)
