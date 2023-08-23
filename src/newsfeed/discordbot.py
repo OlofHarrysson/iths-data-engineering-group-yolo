a6fe3cdee7820955dad46decd8d206d3fb1e85b5
razjoh <raz.johansson@gmail.com>
2023-08-23 19:52:15 +0200

    prepped discord bot for summary

--- src/newsfeed/discordbot.py
+++ src/newsfeed/discordbot.py
@@ -1,8 +1,11 @@
 from discord import SyncWebhook
 
+group = "iths-data-engineering-group-yolo"
+title = "Placeholder for blogtitle"
+summary = "Placeholder for summary"
+info = "Placeholder for additional info"
+
 webhook = SyncWebhook.from_url(
     "https://discord.com/api/webhooks/1131522847509069874/Lwk1yVc4w623xpRPkKYu9faFdMNvV5HTZ3TCcL5DgsIgeqhEvo9tBookvuh2S4IWysTt"
 )
-webhook.send(
-    "Group: iths-data-engineering-group-yolo\nBlog Title: Placeholder for blogtitle\nSummary: Placeholder for summary\nAdditional Info: Placeholder for additional info"
-)
+webhook.send(f"Group: {group}\nBlog Title: {title}\nSummary: {summary}\nAdditional Info: {info}")
