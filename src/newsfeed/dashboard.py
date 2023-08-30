3145968f034aecf25c1c99111cf09e9c91ae145a
Razjoh <112379226+Razjoh@users.noreply.github.com>
2023-08-30 11:08:26 +0200

    43 dashboard start up (#61)
    * basic function dash working
    
    * added search function and slight improvement
    
    * change model to summerize_text (#40)
    
    * change model to summerize_text
    
    * print blog summaries
    
    ---------
    
    Co-authored-by: root <root@DESKTOP-URA6TOB>
    
    * made changes to download two blogs (#53)
    
    * made changes to download two blogs
    
    * made changes on default for both blogs
    
    ---------
    
    Co-authored-by: root <root@DESKTOP-URA6TOB>
    
    * 48 save each summerized article as a file (#55)
    
    * created .py file for saving summarized articles and map for output
    
    * loop to iterate through warehouse summaries and create new file path
    
    * created function to save summerized articles, added folder to .gitignore
    
    * changed gitignore
    
    * extract the two articles with their blog names (#58)
    
    Co-authored-by: root <root@DESKTOP-URA6TOB>
    
    * Implemented code to check if summary already exists, if  yes - don't sum  article again (#59)
    
    ---------
    
    Co-authored-by: Vineela Nedunuri <112384275+VineelaNedunuri@users.noreply.github.com>
    Co-authored-by: root <root@DESKTOP-URA6TOB>
    Co-authored-by: Sara Gunnars <gunnars.sara@gmail.com>

---
+++ src/newsfeed/dashboard.py
@@ -0,0 +1,62 @@
+import json
+import os
+
+import dash
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
+
+app.layout = dbc.Container(
+    [
+        dbc.Row(dbc.Col(html.H1("Updates in the world of AI")), class_name="mt-3"),
+        dbc.Row(
+            [dbc.Col([html.Label("search Title:"), dbc.Input(id="input", value="", type="text")])]
+        ),
+        dbc.Card(dbc.CardBody(html.H1("Articles")), class_name="mt-3"),
+        html.Button("Refresh", id="load-button"),
+        html.Div(id="summaries-container"),
+    ]
+)
+
+
+@app.callback(
+    Output("summaries-container", "children"),
+    [Input("load-button", "n_clicks"), Input("input", "value")],
+)
+def load_smmaries(n_clicks, search_value):
+    if n_clicks is None:
+        return []
+
+    summeries_path = "data/data_warehouse/mit/summaries"
+
+    summeries = [file for file in os.listdir(summeries_path) if file.endswith(".json")]
+    summery_components = []
+
+    for summery in summeries:
+        with open(os.path.join(summeries_path, summery), "r") as summery_file:
+            json_data = json.load(summery_file)
+
+        title = json_data.get("title", "missing title")
+        text = json_data.get("text", "missing text")
+
+        if search_value.lower() not in title.lower():
+            continue
+
+        summery_component = dbc.Card(
+            [
+                dbc.CardHeader(html.H2(title)),
+                dbc.CardBody(html.P(text)),
+            ],
+            class_name="mb-3",
+        )
+
+        summery_components.append(summery_component)
+
+    return summery_components
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
