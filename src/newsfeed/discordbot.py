8e2ee95dae96ebd9d9e5f6f1ff676caec5d48332
root <root@DESKTOP-URA6TOB>
2023-08-31 19:31:14 +0200

    send an embed with a discord bot to make the blogs more readable

--- src/newsfeed/discordbot.py
+++ src/newsfeed/discordbot.py
@@ -11,10 +11,10 @@ WEBHOOK_URL = "https://discord.com/api/webhooks/1131522847509069874/Lwk1yVc4w623
 
 def create_embed(blog_name, title, text, link):
     embed = discord.Embed(
-        title=title, url="https://news.mit.edu/", description=text, color=discord.Color.blue()
+        title=title, url=link, description=text, color=discord.Color.blue()  # color=0xFF5733
     )
-    embed.set_author(name=blog_name)
-    embed.set_footer(text="iths-data-engineering-group-yolo")
+    embed.set_author(name=blog_name.upper(), url="https://news.mit.edu/")
+    embed.set_footer(text=" ⭐ Presented by : iths-data-engineering-group-yolo ⭐")
     return embed
 
 
@@ -27,11 +27,12 @@ def main(blog_name):
     summaries_path = Path("data/data_warehouse") / blog_name / "summaries"
     summaries = [file for file in os.listdir(summaries_path) if file.endswith(".json")]
 
-    first_summary = summaries[0]
+    first_summary = summaries[2]
     with open(os.path.join(summaries_path, first_summary), "r") as f:
         json_data = json.load(f)
 
         title = json_data.get("title", "missing title")
+        print(title)
         text = json_data.get("text", "missing text")
         link = json_data.get("link", "missing link")
         embed = create_embed(blog_name, title, text, link)
@@ -47,29 +48,3 @@ def parse_args():
 if __name__ == "__main__":
     args = parse_args()
     main(blog_name=args.blog_name)
-
-
-# if __name__ == "__main__":
-# path to summeries
-# summeries_path = "data/data_warehouse/mit/summaries"
-
-# makes list of json files @path
-# summeries = [file for file in os.listdir(summeries_path) if file.endswith(".json")]
-
-# loops through summeries
-# for summery in summeries:
-#     with open(summeries_path + "/" + summery, "r") as summery_file:
-#         json_data = json.load(summery_file)
-
-# extracts title and text from json file, if no title or text exist sends default message
-# title = json_data.get("title", "missing title")
-# text = json_data.get("text", "missing text")
-
-
-# formats string and then send it using discord webhook
-# message = f":wave::nerd: \n ***Group:*** iths-data-engineering-group-yolo \n:book:\n ***Blog Title:*** {title} \n:page_facing_up:\n ***Summary:*** {text}"
-
-# webhook = SyncWebhook.from_url(
-#     "https://discord.com/api/webhooks/1131522847509069874/Lwk1yVc4w623xpRPkKYu9faFdMNvV5HTZ3TCcL5DgsIgeqhEvo9tBookvuh2S4IWysTt"
-# )
-# webhook.send(message)
