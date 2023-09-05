d7ef993252d89340d701114e6cf632a9dfba4b7b
SCGlass <sam.charles.glass@gmail.com>
2023-09-05 20:53:59 +0200

    Trying to get psycopg2 to work

--- src/newsfeed/database.py
+++ src/newsfeed/database.py
@@ -3,19 +3,27 @@ import json
 import psycopg2
 
 # read JSON data from file
-with open("data.json") as f:
+data = "data/data_warehouse/mit/summaries/A_better_way_to_study_ocean_currents.json"
+with open(data) as f:
     data = json.load(f)
 
 # connect to the database
-conn = psycopg2.connect(host="localhost", database="mydb", user="user", password="pass")
+conn = psycopg2.connect(host="localhost", database="postgres", user="postgres", password="GAIS1894")
 
-# create a cursor
+# create a cursor used to excute the commits
 cur = conn.cursor()
 
 # execute the INSERT statement
 cur.execute(
-    "INSERT INTO table_name (name, email, city) VALUES (%s, %s, %s)",
-    (data["name"], data["email"], data["city"]),
+    "INSERT INTO table_name (uninque_id, title, text, simple, link, published) VALUES (%s, %s, %s, %s, %s, %s)",
+    (
+        data["unique_id"],
+        data["title"],
+        data["text"],
+        data["simple"],
+        data["link"],
+        data["published"],
+    ),
 )
 
 # commit the changes to the database
