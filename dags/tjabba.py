5c4b182c36e8d73684525b09a006a6fc367e0719
Sara <gunnars.sara@gmail.com>
2023-09-06 12:51:01 +0200

    created a dag and tested it's function

--- dags/tjabba.py
+++ dags/tjabba.py
@@ -1,1 +1,16 @@
-from airflow import DAG, task
+from datetime import datetime
+
+from airflow import DAG
+from airflow.decorators import dag, task
+
+
+@dag(dag_id="test_tjabba", schedule_interval=None, start_date=datetime(2023, 9, 6), catchup=False)
+def test_tjabba_dag():
+    @task
+    def test_tjabba():
+        print("Tjabba")
+
+    test_task = test_tjabba()
+
+
+test_tjabba_dag = test_tjabba_dag()
