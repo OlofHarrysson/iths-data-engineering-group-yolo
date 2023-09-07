ceb1b45595ad9857a05c3adea9f692d4f5f04df5
SCGlass <sam.charles.glass@gmail.com>
2023-09-07 14:45:03 +0200

    stuck on problem on summarize airflow

--- dags/pipeline.py
+++ dags/pipeline.py
@@ -1,21 +1,26 @@
 from datetime import datetime
 
-from airflow.decorators import dag, task, task_group
+from airflow.decorators import dag, task
 
-from newsfeed import download_blogs_from_rss, extract_articles
-
-# from newsfeed import summarize
+from newsfeed import download_blogs_from_rss, extract_articles, summarize
 
 
-@task(task_id="download_blogs_from_rss_task")
-def download_blogs_from_rss_task():
+@task(task_id="download_blogs_from_rss_task", provide_context=True)
+def download_blogs_from_rss_task(**kwargs):
     download_blogs_from_rss.main(blog_name="mit")
     download_blogs_from_rss.main(blog_name="big_data")
 
 
-@task(task_id="extract_articles_task")
-def extract_articles_task():
+@task(task_id="extract_articles_task", provide_context=True)
+def extract_articles_task(**kwargs):
     extract_articles.main(blog_name="mit")
+    extract_articles.main(blog_name="big_data")
+
+
+@task(task_id="summarize_task", provide_context=True)
+def summarize_task(**kwargs):
+    summarize.main(blog_name="mit", args=kwargs)
+    # summarize.main()
 
 
 @dag(
@@ -25,7 +30,7 @@ def extract_articles_task():
     catchup=False,
 )
 def pipeline():
-    download_blogs_from_rss_task() >> extract_articles_task()
+    download_blogs_from_rss_task() >> extract_articles_task() >> summarize_task()
 
 
 pipeline = pipeline()
