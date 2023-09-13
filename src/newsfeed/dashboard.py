35d1b2b2caf6f85081994aca132be499aeb52bdf
Razjoh <112379226+Razjoh@users.noreply.github.com>
2023-09-13 18:13:23 +0200

    82 redesign dashboard (#126)
    * improved utils load file and dash structure
    
    * fixed discord bot bricking
    
    * improved dash and added styling with css
    
    * fixed styling

--- src/newsfeed/dashboard.py
+++ src/newsfeed/dashboard.py
@@ -1,41 +1,51 @@
-import json
-import os
-
 import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-from utils import load_files
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
+from newsfeed.utils import load_files
+
+# themes DARKLY, CYBORG, QUARTZ, MORPH, SKETCHY, SLATE, SOLAR
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.QUARTZ])
 
 app.layout = dbc.Container(
     [
-        dbc.Row(dbc.Col(html.H1("News in the world of AI")), class_name="mt-3"),
-        dbc.Row(
-            [dbc.Col([html.Label("Search Title:"), dbc.Input(id="input", value="", type="text")])]
-        ),
-        dbc.Card(dbc.CardBody(html.H1("Articles")), class_name="mt-3"),
+        dbc.Card(dbc.CardBody(html.H1("News in the world of AI")), class_name="mt-3"),
         dbc.Col(
             dcc.Tabs(
                 id="blog-tabs",
                 value="tab-1",
                 children=[
                     dcc.Tab(
-                        label="mit",
+                        label="Technical",
                         value="tab-1",
                         className="custom-tab",
                         selected_className="custom-tab--selected",
                     ),
                     dcc.Tab(
-                        label="big_data",
+                        label="Non-Technical",
                         value="tab-2",
                         className="custom-tab",
                         selected_className="custom-tab--selected",
                     ),
                 ],
             )
+        ),
+        dbc.Row(
+            [dbc.Col([html.H5("Search Title:"), dbc.Input(id="input", value="", type="text")])]
+        ),
+        dbc.Col(html.H5("Blog:")),
+        dbc.Col(
+            dcc.Dropdown(
+                id="blog-picker",
+                options=[
+                    {"label": option[0], "value": option[1]}
+                    for option in (("MIT", "mit"), ("Big data", "big_data"))
+                ],
+                value="mit",
+            ),
+            class_name="mb-3",
         ),
         html.Div(id="summaries-container"),
     ]
@@ -44,18 +54,18 @@ app.layout = dbc.Container(
 
 @app.callback(
     Output("summaries-container", "children"),
-    [Input("blog-tabs", "value"), Input("input", "value")],
+    [Input("blog-tabs", "value"), Input("input", "value"), Input("blog-picker", "value")],
 )
-def load_smmaries(tab, search_value):
+def load_smmaries(tab, search_value, blog):
     if tab == "tab-1":
-        blog = "mit"
+        content = "text"
     elif tab == "tab-2":
-        blog = "big_data"
+        content = "simple"
 
     summeries_path = "data/data_warehouse/" + blog + "/summaries"
     summery_components = []
 
-    articles = load_files(summeries_path)
+    articles = load_files(summeries_path)[:16]
 
     for article in articles:
         if search_value.lower() not in article["title"].lower():
@@ -67,8 +77,7 @@ def load_smmaries(tab, search_value):
                 dbc.CardBody(
                     html.Div(
                         [
-                            html.P(article["text"]),
-                            html.P(article["simple"]),
+                            html.P(article[content]),
                             html.A("Read full article", href=article["link"], target="blank"),
                             html.P(article["date"]),
                         ]
@@ -80,15 +89,22 @@ def load_smmaries(tab, search_value):
 
         summery_components.append(summery_component)
 
-    # return summery_components
-
     row_components = []
 
     for i in range(0, len(summery_components), 2):
-        row = dbc.Row(
-            [dbc.Col(summery_components[i], width=6), dbc.Col(summery_components[i + 1], width=6)],
-            class_name="mb-3",
-        )
+        if i + 1 < len(summery_components):
+            row = dbc.Row(
+                [
+                    dbc.Col(summery_components[i], width=6),
+                    dbc.Col(summery_components[i + 1], width=6),
+                ],
+                class_name="mb-3",
+            )
+        else:
+            row = dbc.Row(
+                [dbc.Col(summery_components[i], width=6), dbc.Col(width=6)],
+                class_name="mb-3",
+            )
 
         row_components.append(row)
 
